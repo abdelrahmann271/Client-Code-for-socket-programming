5fb7df3346d55a0027284404e0e173967e86ef21
Abdelrahman Nour <abdelrahmann271@gmail.com>
2021-11-08 05:20:15 +0200

    adding stuff

---
+++ http_parser.h
@@ -0,0 +1,149 @@
+//
+// Created by Abdelrahman Nour on 11/4/2021.
+//
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <winsock.h>
+#include <iostream>
+#include <fstream>
+#include <bits/stdc++.h>
+using std::ofstream;
+using namespace std;
+
+
+class HTTP{
+    string status="";
+    string method = "";
+    unordered_map<string,string> headers;
+    string body="";
+    string filename;
+
+public:
+    HTTP(){
+
+    }
+    void set_method(string method_to_send){
+        method = method_to_send;
+    }
+    void set_filename(string file){
+        filename = file;
+    }
+
+    void set_status(int status_code){
+        status = to_string(status_code);
+    }
+    void add_header(string header_name , string header_value){
+        headers[header_name] = header_value;
+    }
+    void  add_body(string message){
+        body = message;
+    }
+    string build(){
+        string http = "";
+        http+=method+" "+filename+" "+"HTTP/1.1\r\n";
+        for (auto i = headers.begin(); i != headers.end(); i++){
+            //cout<<i->first<<": "<<i->second<<"\r\n";
+            http+=i->first+": "+i->second+"\r\n";
+        }
+        http+="\r\n";
+        http+=body;
+        //cout<<http<<" henaaa\n";
+        return http;
+    }
+
+
+};
+
+int size_of_message(char* buffer){
+    int g = 0;
+    while(buffer[g] != '\0'){
+        g++;
+    }
+    return g;
+}
+string convertToString(char* a, int size)
+{
+    int i;
+    string s = "";
+    for (i = 0; i < size; i++) {
+        s = s + a[i];
+    }
+    return s;
+}
+
+vector<char *> get_lines(char *buffer){
+    string s = buffer;
+    vector<char *> lines;
+    char *line = strtok(buffer, "\r\n");
+    //lines.push_back(line);
+    while (line != NULL)
+    {
+        lines.push_back(line);
+        line = strtok(NULL, "\r\n");
+
+    }
+    return lines;
+}
+
+vector<char *> get_words(char * line){
+    vector<char *> words;
+    char *word = strtok(line, " ");
+    //words.push_back(word);
+    while (word != NULL)
+    {
+        words.push_back(word);
+        word = strtok(NULL, " ");
+
+    }
+    return words;
+}
+
+void parse_http(char *message ,
+                unordered_map<string,string> &headers ,
+                string &http_version ,
+                string &status_code ,
+                string &body ,
+                int size_of_message){
+
+//    cout<<size_of_message<<"\n";
+    int i =0;
+    int end_of_headers;
+    for( ; i < size_of_message ; i++){
+        if(message[i] == '\r' && message[i+1] == '\n' && message[i+2] == '\r' && message[i+3] == '\n'){
+            end_of_headers = i-1;
+            break;
+        }
+    }
+    int k = 0 ;
+    char msg[end_of_headers+1];
+    while(k!=end_of_headers){
+        msg[k] = message[k];
+        k++;
+    }
+    msg[k]='\0';
+    int j = i+4;
+    while(message[j] != '\0' ){
+        body+=message[j];
+        j++;
+    }
+    vector<char *> lines = get_lines(msg);
+    vector<char *> words = get_words(lines[0]);
+
+    http_version  = words[0];
+    status_code = words[1];
+
+    char *word;
+    for(int i = 1 ; i < lines.size() ; i++){
+        vector<char *> words;
+        word= strtok(lines[i], ": ");
+        while (word != NULL)
+        {
+            words.push_back(word);
+            word = strtok(NULL, ": ");
+        }
+        headers[words[0]] = words[1];
+    }
+
+
+}
--- main.cpp
+++ main.cpp
@@ -5,13 +5,19 @@
 #include <iostream>
 #include <fstream>
 #include <bits/stdc++.h>
+#include "http_parser.h"
 using std::ofstream;
 using namespace std;
 
 #define DEST_PORT 4000
 #define DEST_IP "127.0.0.1"
+#define DEFAULT_BUFLEN 8196
 //cout<<"==>"<<<<"\n";
 int main() {
+
+
+    //std::ifstream input( "eye.png", std::ios::binary );
+
 
 
     WSAData wsaData;
@@ -31,6 +37,11 @@ int main() {
     }
     cout<<"==>"<<"Socket Assignment Success"<<"\n";
 
+
+    //Read from File
+    //then execute commands..
+    //For Every Command do the following ..
+
     dest_addr.sin_family = AF_INET; // host byte order
     dest_addr.sin_port = htons(DEST_PORT); // short, network byte order
     dest_addr.sin_addr.s_addr = inet_addr(DEST_IP);
@@ -44,28 +55,123 @@ int main() {
     }
     cout<<"==>"<<"Client is connected to Server"<<"\n";
 
-
+    string method = "POST"; //or "GET" from command
+    string filename = "/test"; // from command
 
     //Send Message
+    HTTP request;
+    request.set_method("POST");
+    request.set_filename(filename);
+    string body;
 
 
-    char *msg = "GET g.txt 192.168.1.68:3001 HTTP/1.1\r\n\r\n";
+    if(method == "GET"){
+
+//        whole_request = head;
+    }
+    else{ // POST
+        string file_to_post =  "hello.html"; // From Command
+        std::ifstream input2( file_to_post , std::ios::binary );
+
+        string str;
+        while(getline(input2,str))
+            body+=str;
+//        for(int i = 0 ; i < body.size() ; i++){
+//            cout<<body[i];
+//        }
+//        cout<<"\n"<<body.size()<<'\n';
+//        whole_request = head+body;
+        request.add_header("Content-Length", to_string(body.size()));
+        request.add_header("Content-Type","text/html"); // From Command
+        request.add_body(body);
+    }
+    string whole_request = request.build();
+    char msg[10000];
+    int i =0;
+    for( ; i < whole_request.size(); i++){
+
+//        if(whole_request[i]=='\0'){
+//            //cout<<"\n7asal\n";
+//            msg[i] = msg[i-1];
+//        }
+//        else{
+            msg[i] = whole_request[i];
+//        }
+        //cout<<msg[i];
+    }
+//    char *msg = "POST /test HTTP/1.1\r\n\r\n";
+
     int len, bytes_sent;
     len = strlen(msg);
-    bytes_sent = send(sockfd, msg, len, 0);
-    if(bytes_sent == -1){
-        perror("bytes_sent error");
+    cout<<"\nlen"<<i<<"\n";
+    int recvbuflen = DEFAULT_BUFLEN;
+    //const char *sendbuf = "this is a test";
+    char recvbuf[DEFAULT_BUFLEN];
+    int iResult;
+// Send an initial buffer
+    iResult = send(sockfd, msg, (int) strlen(msg), 0);
+    if (iResult == SOCKET_ERROR) {
+        printf("send failed: %d\n", WSAGetLastError());
+        closesocket(sockfd);
+        WSACleanup();
+        return 1;
     }
+    printf("Bytes Sent: %ld\n", iResult);
+    iResult = send(sockfd, msg, (int) strlen(msg), 0);
+    if (iResult == SOCKET_ERROR) {
+        printf("send failed: %d\n", WSAGetLastError());
+        closesocket(sockfd);
+        WSACleanup();
+        return 1;
+    }
+    printf("Bytes Sent: %ld\n", iResult);
+// shutdown the connection for sending since no more data will be sent
+// the client can still use the ConnectSocket for receiving data
+
+//    bytes_sent = send(sockfd, msg,i, 0);
+//    cout<<bytes_sent<<"\n";
+//    if(bytes_sent == -1){
+//        perror("bytes_sent error");
+//    }
     cout<<"==>"<<"message is sent to Server"<<"\n";
     //Receive Message
-    char buf[2096];
-    int bytesReceived = recv(sockfd, buf, 2096, 0);
+    char buf[DEFAULT_BUFLEN]={0};
+
+    int bytesReceived = recv(sockfd, buf, DEFAULT_BUFLEN, 0);
     if (bytesReceived > 0)
     {
-        // Echo response to console
-        cout << "SERVER> " << string(buf, 0, bytesReceived);
-        cout<<"==>"<<"Response Received Successfully"<<"\n";
+        cout<<"==>"<<"The messgage"<<"\n";
+        cout<<buf<<"\n";
+        unordered_map<string,string> headers;
+        string http_version;
+        string body;
+        string status_code;
+        parse_http(buf, headers,http_version,status_code,body,size_of_message(buf));
+//        cout<<buf<<"\n";
+//        cout<<"==>"<<"http_version\n";
+//        cout<<http_version<<"\n";
+//        cout<<"==>"<<"status code\n";
+//        cout<<status_code<<"\n";
+//        cout<<"==>"<<"body\n";
+//        cout<<body<<"\n";
+        if(method == "GET"){
+            string file_name = "yoyo.html";
+            // std::ofstream output( file_name, std::ios::binary );
+            std::ofstream output;
+            output.open(file_name);
+            output<<body;
+            cout<<"==>"<<"Response Received Successfully"<<"\n";
+        }
+
     }
 
+//    iResult = shutdown(sockfd, 2);
+//    if (iResult == SOCKET_ERROR) {
+//        printf("shutdown failed: %d\n", WSAGetLastError());
+//        closesocket(sockfd);
+//        WSACleanup();
+//        return 1;
+//    }
+    cout<<"\nSsafe";
     return 0;
 }
